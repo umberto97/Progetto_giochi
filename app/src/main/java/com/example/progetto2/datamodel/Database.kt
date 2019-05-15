63222b97447945070b1bbab237e86a8bf57b2c2a
GiustFR98 <giustino.esposito@studenti.unicampania.it>
2019-05-15 21:39:55 +0200

    Creata classe gioco e simulatore Database

---
+++ app/src/main/java/com/example/progetto2/datamodel/Database.kt
@@ -0,0 +1,9 @@
+package com.example.progetto2.datamodel
+
+object Database {
+
+    private var Giochi=ArrayList<Gioco>()
+    init {
+            Giochi.add(Gioco("Red Dead Redemption 2", 30, "Aversa"))
+    }
+}
