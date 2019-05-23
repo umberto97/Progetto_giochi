d416657f08828e82daff8b51408d9173d3351a4d
umberto <umberto.gagliardini@studenti.unicampania.it>
2019-05-23 23:00:36 +0200

    Aggiornato fragment inserimento (abilitata fotocamera)

--- app/src/main/java/com/example/progetto2/fragment_inserimento.kt
+++ app/src/main/java/com/example/progetto2/fragment_inserimento.kt
@@ -1,41 +1,20 @@
 package com.example.progetto2
 
-import android.content.Context
-import android.net.Uri
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_fragment_inserimento.*
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [fragment_inserimento.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [fragment_inserimento.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class fragment_inserimento : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // Costante utilizzata per distinguere l'origine della richiesta
+    val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,50 +24,27 @@ class fragment_inserimento : Fragment() {
         return inflater.inflate(R.layout.fragment_fragment_inserimento, container, false)
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // Imposta il funzionamento del pulsante per l'acqisizione dell'immagine
+        foto1.setOnClickListener {
+            // Creo un intent di tipo implicito per acquisire l'immagine
+            val takePhoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            takePhoto.resolveActivity(activity!!.packageManager)?.also {
+                startActivityForResult(takePhoto, REQUEST_IMAGE_CAPTURE)
+            }
+        }
     }
 
     /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
+     * Questo metodo viene invocato per gestire il risultato al ritorno da una activity
+     * occorre determinare chi aveva generato la richiesta
      */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {     // Acquisizione immagine
+            val immagineCatturata = data?.extras?.get("data") as Bitmap
+            foto1.setImageBitmap(immagineCatturata)
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_inserimento.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_inserimento().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
